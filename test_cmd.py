4da7856d72bdd5a9751a7727d13e618a40792dd9
Alexander Solovyov <piranha@piranha.org.ua>
2009-07-18 00:03:35 +0300

    more tests

---
+++ test_cmd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import finaloption
+
+config_opts=[('c', 'config', 'webshops.ini', 'config file to use')]
+
+
+@finaloption.command(config_opts)
+def initdb(config):
+    """Initialize database"""
+    pass
+
+
+@finaloption.command(options=config_opts + [
+    ('h', 'host', 'localhost', 'The host for the application.'),
+    ('p', 'port', 5000, 'The port for the server.'),
+    ('', 'nolint', False, 'Do not use LintMiddleware')
+])
+def runserver(config, host, port, nolint):
+    """Run development server"""
+
+    pass
+
+
+finaloption.dispatch()
