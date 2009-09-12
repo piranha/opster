50c912e9697dcab2091380bc40cf5e33b3ad3578
Alexander Solovyov <piranha@piranha.org.ua>
2009-09-12 08:54:47 +0300

    test_cmd.py update to reflect updates in opster

--- test_cmd.py
+++ test_cmd.py
@@ -4,22 +4,17 @@ import opster
 
 config_opts=[('c', 'config', 'webshops.ini', 'config file to use')]
 
-
 @opster.command(config_opts)
 def initdb(config):
     """Initialize database"""
     pass
 
-
-@opster.command(options=config_opts + [
-    ('h', 'host', 'localhost', 'The host for the application.'),
-    ('p', 'port', 5000, 'The port for the server.'),
-    ('', 'nolint', False, 'Do not use LintMiddleware')
-])
-def runserver(**opts):
+@opster.command(config_opts)
+def runserver(listen=('l', 'localhost', 'ip to listen on'),
+              port=('p', 5000, 'port to listen on'),
+              **opts):
     """Run development server"""
-    print opts
-
+    print locals()
 
 if __name__ == '__main__':
     opster.dispatch()
