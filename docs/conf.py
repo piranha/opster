23993216c4123fd45bad18cb5b27e1a5f8c3635e
Alexander Solovyov <alexander@solovyov.net>
2011-08-16 11:02:25 +0200

    convert hgignore to gitignore

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,6 @@ exclude_trees = ['_build']
 
 # -- Options for HTML output ---------------------------------------------------
 
-html_theme = 'cleanery'
-html_theme_path = ['.']
+# html_theme = 'cleanery'
+# html_theme_path = ['.']
 html_title = "%s v%s" % (project, version)
